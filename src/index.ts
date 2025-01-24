61ae5c90326b57e6d2658c806c293e9ed57eb9e2
AnkitBhatt <ankitbhatt5597@gmail.com>
2025-01-24 22:17:15 +0530

    sum function defined without argument and still gives zero as result

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import { sum } from './sumController';
 
 const app = express();
+
+app.use(express.json())
 
 //view engine
 app.set('view engine', 'ejs');
@@ -11,6 +14,10 @@ app.get('/', (req, res) => {
 
 app.post('/sum', (req, res)=>{
   //will handle sum function here
+  const {stringInput} = req.body;
+  let output = sum();
+  
+  res.status(200).send({result:output})
 });
 
 app.listen(process.env.PORT, ()=>{
--- src/sumController.test.ts
+++ src/sumController.test.ts
@@ -1,4 +1,5 @@
 import {describe, expect, test} from '@jest/globals';
+import {sum} from './sumController'
 
 describe('Sum Controller', () => {
   test('should return 0 for no input', () => {
